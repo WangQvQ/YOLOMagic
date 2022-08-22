e760595a37456c15e139bd35f4afbc3c4c3182bb
白屋 <1579093407@qq.com>
2022-08-22 10:28:40 +0800

    增加了中文注释

--- models/yolo.py
+++ models/yolo.py
@@ -5,13 +5,16 @@ YOLO-specific modules
 Usage:
     $ python path/to/models/yolo.py --cfg yolov5s.yaml
 """
+# 代码中文注释, 参考了CSDN的【满船清梦压星河HK】大佬的博文
+# https://blog.csdn.net/qq_38253797/article/details/119869762
 
-import argparse
-import os
-import platform
-import sys
-from copy import deepcopy
-from pathlib import Path
+
+import argparse  # 解析命令行参数模块
+import os  # 一些基本的操作系统交互功能
+import platform  # 获取操作系统详细信息
+import sys  # sys系统模块 包含了与Python解释器和它的环境有关的函数
+from copy import deepcopy  # 数据拷贝模块 深拷贝
+from pathlib import Path  # Path将str转换为Path对象 使字符串路径易于操作的模块
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
@@ -28,6 +31,7 @@ from utils.plots import feature_visualization
 from utils.torch_utils import (fuse_conv_and_bn, initialize_weights, model_info, profile, scale_img, select_device,
                                time_sync)
 
+# 导入thop包 用于计算FLOPs
 try:
     import thop  # for FLOPs computation
 except ImportError:
@@ -35,47 +39,79 @@ except ImportError:
 
 
 class Detect(nn.Module):
+    """Detect模块是用来构建Detect层的，将输入feature map
+    通过一个卷积操作和公式计算到我们想要的shape, 为后面的计算损失或者NMS作准备"""
     stride = None  # strides computed during build
     onnx_dynamic = False  # ONNX export parameter
     export = False  # export mode
 
     def __init__(self, nc=80, anchors=(), ch=(), inplace=True):  # detection layer
         super().__init__()
-        self.nc = nc  # number of classes
-        self.no = nc + 5  # number of outputs per anchor
-        self.nl = len(anchors)  # number of detection layers
-        self.na = len(anchors[0]) // 2  # number of anchors
-        self.grid = [torch.zeros(1)] * self.nl  # init grid
+        """
+        detection layer 相当于yolov3中的YOLOLayer层
+        params nc: number of classes
+        :params anchors: 传入3个feature map上的所有anchor的大小（P3、P4、P5）
+        :params ch: [128, 256, 512] 3个输出feature map的channel
+        """
+        self.nc = nc  # number of classes VOC: 20
+        self.no = nc + 5  # number of outputs per anchor VOC: 5+20=25  xywhc+20classes
+        self.nl = len(anchors)  # number of detection layers Detect的个数 3
+        self.na = len(anchors[0]) // 2  # number of anchors 每个feature map的anchor个数 3
+        self.grid = [torch.zeros(1)] * self.nl  # init grid {list: 3}  tensor([0.]) X 3
         self.anchor_grid = [torch.zeros(1)] * self.nl  # init anchor grid
+        # register_buffer
+        # 模型中需要保存的参数一般有两种：一种是反向传播需要被optimizer更新的，称为parameter; 另一种不要被更新称为buffer
+        # buffer的参数更新是在forward中，而optim.step只能更新nn.parameter类型的参数
+        # shape(nl,na,2)
         self.register_buffer('anchors', torch.tensor(anchors).float().view(self.nl, -1, 2))  # shape(nl,na,2)
+        # output conv 对每个输出的feature map都要调用一次conv1x1
         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
+        # use in-place ops (e.g. slice assignment) 一般都是True 默认不使用AWS Inferentia加速
         self.inplace = inplace  # use in-place ops (e.g. slice assignment)
 
     def forward(self, x):
+        """
+        :return train: 一个tensor list 存放三个元素   [bs, anchor_num, grid_w, grid_h, xywh+c+20classes]
+                       分别是 [1, 3, 80, 80, 25] [1, 3, 40, 40, 25] [1, 3, 20, 20, 25]
+                       inference: 0 [1, 19200+4800+1200, 25] = [bs, anchor_num*grid_w*grid_h, xywh+c+20classes]
+                                  1 一个tensor list 存放三个元素 [bs, anchor_num, grid_w, grid_h, xywh+c+20classes]
+                                    [1, 3, 80, 80, 25] [1, 3, 40, 40, 25] [1, 3, 20, 20, 25]
+        """
         z = []  # inference output
-        for i in range(self.nl):
-            x[i] = self.m[i](x[i])  # conv
+        for i in range(self.nl):  # 对三个feature map分别进行处理
+            x[i] = self.m[i](x[i])  # conv  xi[bs, 128/256/512, 80, 80] to [bs, 75, 80, 80]
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+            # [bs, 75, 80, 80] to [1, 3, 25, 80, 80] to [1, 3, 80, 80, 25]
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
             if not self.training:  # inference
+                # 构造网格
+                # 因为推理返回的不是归一化后的网格偏移量 需要再加上网格的位置 得到最终的推理坐标 再送入nms
+                # 所以这里构建网格就是为了纪律每个grid的网格坐标 方面后面使用
                 if self.onnx_dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
 
                 y = x[i].sigmoid()
                 if self.inplace:
+                    # 默认执行 不使用AWS Inferentia
+                    # 这里的公式和yolov3、v4中使用的不一样 是yolov5作者自己用的 效果更好
                     y[..., 0:2] = (y[..., 0:2] * 2 + self.grid[i]) * self.stride[i]  # xy
-                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                    # y[..., 2:4] = torch.exp(y[..., 2:4]) * self.anchor_wh     # wh yolo method
+                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh power method
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
                     xy, wh, conf = y.split((2, 2, self.nc + 1), 4)  # y.tensor_split((2, 4, 5), 4)  # torch 1.8.0
                     xy = (xy * 2 + self.grid[i]) * self.stride[i]  # xy
                     wh = (wh * 2) ** 2 * self.anchor_grid[i]  # wh
                     y = torch.cat((xy, wh, conf), 4)
+                # z是一个tensor list 三个元素 分别是[1, 19200, 25] [1, 4800, 25] [1, 1200, 25]
                 z.append(y.view(bs, -1, self.no))
 
         return x if self.training else (torch.cat(z, 1),) if self.export else (torch.cat(z, 1), x)
 
     def _make_grid(self, nx=20, ny=20, i=0):
+        """
+        构造网格
+        """
         d = self.anchors[i].device
         t = self.anchors[i].dtype
         shape = 1, self.na, ny, nx, 2  # grid shape
@@ -93,43 +129,62 @@ class Model(nn.Module):
     # YOLOv5 model
     def __init__(self, cfg='yolov5s.yaml', ch=3, nc=None, anchors=None):  # model, input channels, number of classes
         super().__init__()
+        """
+        :params cfg:模型配置文件
+        :params ch: input img channels 一般是3 RGB文件
+        :params nc: number of classes 数据集的类别个数
+        :anchors: 一般是None
+        """
         if isinstance(cfg, dict):
             self.yaml = cfg  # model dict
         else:  # is *.yaml
             import yaml  # for torch hub
-            self.yaml_file = Path(cfg).name
+            self.yaml_file = Path(cfg).name  # cfg file name = yolov5s.yaml
+            # 如果配置文件中有中文，打开时要加encoding参数
             with open(cfg, encoding='ascii', errors='ignore') as f:
+                # model dict  取到配置文件中每条的信息（没有注释内容）
                 self.yaml = yaml.safe_load(f)  # model dict
 
         # Define model
         ch = self.yaml['ch'] = self.yaml.get('ch', ch)  # input channels
+        # 设置类别数 一般不执行, 因为nc=self.yaml['nc']恒成立
         if nc and nc != self.yaml['nc']:
             LOGGER.info(f"Overriding model.yaml nc={self.yaml['nc']} with nc={nc}")
             self.yaml['nc'] = nc  # override yaml value
+        # 重写anchor，一般不执行, 因为传进来的anchors一般都是None
         if anchors:
             LOGGER.info(f'Overriding model.yaml anchors with anchors={anchors}')
             self.yaml['anchors'] = round(anchors)  # override yaml value
+        # 创建网络模型
+        # self.model：初始化的整个网络模型(包括Detect层结构)
+        # self.save：所有层结构中from不等于-1的序号，并排好序 [4, 6, 10, 14, 17, 20, 23]
         self.model, self.save = parse_model(deepcopy(self.yaml), ch=[ch])  # model, savelist
+        # default class names ['0', '1', '2',..., '19']
         self.names = [str(i) for i in range(self.yaml['nc'])]  # default names
-        self.inplace = self.yaml.get('inplace', True)
+        self.inplace = self.yaml.get('inplace', True)  # 默认True  不使用加速推理
 
         # Build strides, anchors
+        # 获取Detect模块的stride(相对输入图像的下采样率)和anchors在当前Detect输出的feature map的尺度
         m = self.model[-1]  # Detect()
         if isinstance(m, Detect):
             s = 256  # 2x min stride
             m.inplace = self.inplace
+            # 计算三个feature map下采样的倍率  [8, 16, 32]
             m.stride = torch.tensor([s / x.shape[-2] for x in self.forward(torch.zeros(1, ch, s, s))])  # forward
             check_anchor_order(m)  # must be in pixel-space (not grid-space)
+            # 求出相对当前feature map的anchor大小 如[10, 13]/8 -> [1.25, 1.625]
             m.anchors /= m.stride.view(-1, 1, 1)
             self.stride = m.stride
-            self._initialize_biases()  # only run once
+            self._initialize_biases()  # only run once   初始化偏置
 
         # Init weights, biases
-        initialize_weights(self)
-        self.info()
+        initialize_weights(self)  # 调用torch_utils.py下initialize_weights初始化模型权重
+        self.info()  # 打印模型信息
         LOGGER.info('')
 
     def forward(self, x, augment=False, profile=False, visualize=False):
+        # augmented inference, None  上下flip/左右flip
+        # 是否在测试时也使用数据增强  Test Time Augmentation(TTA)
         if augment:
             return self._forward_augment(x)  # augmented inference, None
         return self._forward_once(x, profile, visualize)  # single-scale inference, train
@@ -140,19 +195,39 @@ class Model(nn.Module):
         f = [None, 3, None]  # flips (2-ud, 3-lr)
         y = []  # outputs
         for si, fi in zip(s, f):
+            # scale_img缩放图片尺寸
             xi = scale_img(x.flip(fi) if fi else x, si, gs=int(self.stride.max()))
             yi = self._forward_once(xi)[0]  # forward
             # cv2.imwrite(f'img_{si}.jpg', 255 * xi[0].cpu().numpy().transpose((1, 2, 0))[:, :, ::-1])  # save
+            # _descale_pred将推理结果恢复到相对原图图片尺寸
             yi = self._descale_pred(yi, fi, si, img_size)
             y.append(yi)
         y = self._clip_augmented(y)  # clip augmented tails
         return torch.cat(y, 1), None  # augmented inference, train
 
     def _forward_once(self, x, profile=False, visualize=False):
+        """
+        :params x: 输入图像
+        :params profile: True 可以做一些性能评估
+        :params feature_vis: True 可以做一些特征可视化
+        :return train: 一个tensor list 存放三个元素   [bs, anchor_num, grid_w, grid_h, xywh+c+20classes]
+                       分别是 [1, 3, 80, 80, 25] [1, 3, 40, 40, 25] [1, 3, 20, 20, 25]
+                inference: 0 [1, 19200+4800+1200, 25] = [bs, anchor_num*grid_w*grid_h, xywh+c+20classes]
+                           1 一个tensor list 存放三个元素 [bs, anchor_num, grid_w, grid_h, xywh+c+20classes]
+                             [1, 3, 80, 80, 25] [1, 3, 40, 40, 25] [1, 3, 20, 20, 25]
+                """
+        # y: 存放着self.save=True的每一层的输出，因为后面的层结构concat等操作要用到
+        # dt: 在profile中做性能评估时使用
         y, dt = [], []  # outputs
         for m in self.model:
+            # 前向推理每一层结构   m.i=index   m.f=from   m.type=类名   m.np=number of params
+            # if not from previous layer   m.f=当前层的输入来自哪一层的输出  s的m.f都是-1
             if m.f != -1:  # if not from previous layer
+                # 这里需要做4个concat操作和1个Detect操作
+                # concat操作如m.f=[-1, 6] x就有两个元素,一个是上一层的输出,另一个是index=6的层的输出 再送到x=m(x)做concat操作
+                # Detect操作m.f=[17, 20, 23] x有三个元素,分别存放第17层第20层第23层的输出 再送到x=m(x)做Detect的forward
                 x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
+            # 打印日志信息  FLOPs time等
             if profile:
                 self._profile_one_layer(m, x, dt)
             x = m(x)  # run
@@ -213,6 +288,9 @@ class Model(nn.Module):
             mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
 
     def _print_biases(self):
+        """
+        打印模型中最后Detect层的偏置bias信息(也可以任选哪些层bias信息)
+        """
         m = self.model[-1]  # Detect() module
         for mi in m.m:  # from
             b = mi.bias.detach().view(m.na, -1).T  # conv.bias(255) to (3,85)
@@ -225,13 +303,19 @@ class Model(nn.Module):
     #             LOGGER.info('%10.3g' % (m.w.detach().sigmoid() * 2))  # shortcut weights
 
     def fuse(self):  # fuse model Conv2d() + BatchNorm2d() layers
-        LOGGER.info('Fusing layers... ')
+        """用在detect.py、val.py
+        fuse model Conv2d() + BatchNorm2d() layers
+        调用torch_utils.py中的fuse_conv_and_bn函数和common.py中Conv模块的fuseforward函数
+        """
+        LOGGER.info('Fusing layers... ')  # 日志
+        # 遍历每一层结构
         for m in self.model.modules():
+            # 如果当前层是卷积层Conv且有bn结构, 那么就调用fuse_conv_and_bn函数讲conv和bn进行融合, 加速推理
             if isinstance(m, (Conv, DWConv)) and hasattr(m, 'bn'):
-                m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
-                delattr(m, 'bn')  # remove batchnorm
-                m.forward = m.forward_fuse  # update forward
-        self.info()
+                m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv # 融合 update conv
+                delattr(m, 'bn')  # remove batchnorm # 移除bn remove batchnorm
+                m.forward = m.forward_fuse  # update forward # 更新前向传播 update forward (反向传播不用管, 因为这种推理只用在推理阶段)
+        self.info()  # 打印conv+bn融合后的模型信息
         return self
 
     def info(self, verbose=False, img_size=640):  # print model information
@@ -250,20 +334,40 @@ class Model(nn.Module):
 
 
 def parse_model(d, ch):  # model_dict, input_channels(3)
+    """用在上面Model模块中
+       解析模型文件(字典形式)，并搭建网络结构
+       这个函数其实主要做的就是: 更新当前层的args（参数）,计算c2（当前层的输出channel） =>
+                             使用当前层的参数搭建当前层 =>
+                             生成 layers + save
+       :params d: model_dict 模型文件 字典形式 {dict:7}  yolov5s.yaml中的6个元素 + ch
+       :params ch: 记录模型每一层的输出channel 初始ch=[3] 后面会删除
+       :return nn.Sequential(*layers): 网络的每一层的层结构
+       :return sorted(save): 把所有层结构中from不是-1的值记下 并排序 [4, 6, 10, 14, 17, 20, 23]
+       """
     LOGGER.info(f"\n{'':>3}{'from':>18}{'n':>3}{'params':>10}  {'module':<40}{'arguments':<30}")
+    # 读取d字典中的anchors和parameters(nc、depth_multiple、width_multiple)
     anchors, nc, gd, gw = d['anchors'], d['nc'], d['depth_multiple'], d['width_multiple']
+    # na: number of anchors 每一个predict head上的anchor数 = 3
     na = (len(anchors[0]) // 2) if isinstance(anchors, list) else anchors  # number of anchors
+    # no: number of outputs 每一个predict head层的输出channel = anchors * (classes + 5) = 75(VOC)
     no = na * (nc + 5)  # number of outputs = anchors * (classes + 5)
 
+    # 开始搭建网络
+    # layers: 保存每一层的层结构
+    # save: 记录下所有层结构中from中不是-1的层结构序号
+    # c2: 保存当前层的输出channel
     layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
+    # from(当前层输入来自哪些层), number(当前层次数 初定), module(当前层类别), args(当前层类参数 初定)
     for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from, number, module, args
+        # eval(string) 得到当前层的真实类名 例如: m= Focus -> <class 'models.common.Focus'>
         m = eval(m) if isinstance(m, str) else m  # eval strings
         for j, a in enumerate(args):
             try:
                 args[j] = eval(a) if isinstance(a, str) else a  # eval strings
             except NameError:
                 pass
-
+        # ------------------- 更新当前层的args（参数）,计算c2（当前层的输出channel） -------------------
+        # depth gain 控制深度  如v5s: n*0.33   n: 当前模块的次数(间接控制深度)
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in (Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
                  BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, C3x,
@@ -273,48 +377,65 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                  ASPP, BasicRFB, SPPCSPC, SPPCSPC_group,
                  C3_CoordAtt_Attention, C3_SE_Attention, C3_ECA_Attention, C3_CBAM_Attention,
                  ):
+            # c1: 当前层的输入的channel数  c2: 当前层的输出的channel数(初定)  ch: 记录着所有层的输出channel
             c1, c2 = ch[f], args[0]
+            # if not output  no=75  只有最后一层c2=no  最后一层不用控制宽度，输出channel必须是no
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
 
+            # 在初始arg的基础上更新 加入当前层的输入channel并更新当前层
+            # [in_channel, out_channel, *args[1:]]
             args = [c1, c2, *args[1:]]
+            # 如果当前层是BottleneckCSP/C3/C3TR, 则需要在args中加入bottleneck的个数
+            # [in_channel, out_channel, Bottleneck的个数n, bool(True表示有shortcut 默认，反之无)]
             if m in [BottleneckCSP, C3, C3TR, C3Ghost, C3x,
                      C3CA, C3ECA, C3CBAM, C3SE,
                      C3_CoordAtt_Attention, C3_SE_Attention, C3_ECA_Attention, C3_CBAM_Attention]:
-                args.insert(2, n)  # number of repeats
-                n = 1
-            elif m is nn.ConvTranspose2d:
+                args.insert(2, n)  # number of repeats  在第二个位置插入bottleneck个数n
+                n = 1  # 恢复默认值1
+            elif m is nn.ConvTranspose2d:  # 转置卷积
                 if len(args) >= 7:
                     args[6] = make_divisible(args[6] * gw, 8)
         elif m is nn.BatchNorm2d:
+            # BN层只需要返回上一层的输出channel
             args = [ch[f]]
         elif m is Concat:
+            # Concat层则将f中所有的输出累加得到这层的输出channel
             c2 = sum(ch[x] for x in f)
         # 添加bifpn_add结构
         elif m in [BiFPN_Add2, BiFPN_Add3]:
             c2 = max([ch[x] for x in f])
-        elif m is Detect:
+        elif m is Detect:  # Detect（YOLO Layer）层
+            # 在args中加入三个Detect层的输出channel
             args.append([ch[x] for x in f])
-            if isinstance(args[1], int):  # number of anchors
+            if isinstance(args[1], int):  # number of anchors   几乎不执行
                 args[1] = [list(range(args[1] * 2))] * len(f)
-        elif m is Contract:
+        elif m is Contract:  # 不怎么用
             c2 = ch[f] * args[0] ** 2
-        elif m is space_to_depth:
+        elif m is space_to_depth:  # SPD模块
             c2 = 4 * ch[f]
-        elif m is Expand:
+        elif m is Expand:  # 不怎么用
             c2 = ch[f] // args[0] ** 2
         else:
-            c2 = ch[f]
+            # Upsample
+            c2 = ch[f]  # args不变
+        # -----------------------------------------------------------------------------------
 
+        # m_: 得到当前层module  如果n>1就创建多个m(当前层结构), 如果n=1就创建一个m
         m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
+
+        # 打印当前层结构的一些基本信息
         t = str(m)[8:-2].replace('__main__.', '')  # module type
-        np = sum(x.numel() for x in m_.parameters())  # number params
+        np = sum(x.numel() for x in m_.parameters())  # number params  计算这一层的参数量
         m_.i, m_.f, m_.type, m_.np = i, f, t, np  # attach index, 'from' index, type, number params
         LOGGER.info(f'{i:>3}{str(f):>18}{n_:>3}{np:10.0f}  {t:<40}{str(args):<30}')  # print
+        # append to savelist  把所有层结构中from不是-1的值记下  [6, 4, 14, 10, 17, 20, 23]
         save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+        # 将当前层结构module加入layers中
         layers.append(m_)
         if i == 0:
-            ch = []
+            ch = []  # 去除输入channel [3]
+        # 把当前层的输出channel数加入ch
         ch.append(c2)
     return nn.Sequential(*layers), sorted(save)
 
