52c475a58d863ea075e5860cd038a3422a3eeb1d
WangQvQ <1579093407@qq.com>
2024-04-25 12:33:30 +0800

    format yaml

--- models/yolo.py
+++ models/yolo.py
@@ -315,14 +315,13 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in {
                 Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
-                BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, SimSPPF, BasicRFB,
-                ASPP, SPPCSPC, SPPFCSPC,
+                BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, 
+                SimSPPF, BasicRFB, ASPP, SPPCSPC, SPPFCSPC,
                 Shuffle_Block, CBRM, MBConvBlock, stem,
                 SE, CBAM, ECA, CoordAtt}:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
-
             args = [c1, c2, *args[1:]]
             if m in {BottleneckCSP, C3, C3TR, C3Ghost, C3x}:
                 args.insert(2, n)  # number of repeats
@@ -336,7 +335,6 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             args = [c2, *args[0:]]
         elif m is Concat:
             c2 = sum(ch[x] for x in f)
-        # TODO: channel, gw, gd
         elif m in {Detect, Segment}:
             args.append([ch[x] for x in f])
             if isinstance(args[1], int):  # number of anchors
